7d6e6b39fefc5828e3c841461a2cf4e446c48d0f
FloJo07 <florian.jost89@gmail.com>
2020-04-09 11:30:45 +0200

    chore: change names components and add icons

--- Karma/src/app/tabs/tabs-routing.module.ts
+++ Karma/src/app/tabs/tabs-routing.module.ts
@@ -8,45 +8,35 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
+        path: 'dashboard',
         children: [
           {
             path: '',
             loadChildren: () =>
-              import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+            import('../Dashboard/dashboard.module').then(m => m.DashboardPageModule)
           }
         ]
       },
       {
-        path: 'tab2',
+        path: 'statistics',
         children: [
           {
             path: '',
             loadChildren: () =>
-              import('../tab2/tab2.module').then(m => m.Tab2PageModule)
-          }
-        ]
-      },
-      {
-        path: 'tab3',
-        children: [
-          {
-            path: '',
-            loadChildren: () =>
-              import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+              import('../Statistics/statistics.module').then(m => m.StatisticsPageModule)
           }
         ]
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/dashboard',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/dashboard',
     pathMatch: 'full'
   }
 ];
