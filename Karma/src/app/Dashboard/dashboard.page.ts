8d2bae8a56c7b56a78d184af285ac54378675d6c
FloJo07 <florian.jost89@gmail.com>
2020-04-10 12:26:22 +0200

    refactor: remove import headercomponent

--- Karma/src/app/Dashboard/dashboard.page.ts
+++ Karma/src/app/Dashboard/dashboard.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { DashboardService } from '../services/dashboard.service';
-import { HeaderComponent } from './components/header/header.component';
 import { Action } from '../action';
 
 @Component({
